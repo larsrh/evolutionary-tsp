7cc2235da2d65cbd283a844f7be7693d24352b40
Lars Hupel <lars.hupel@mytum.de>
2011-09-27 11:49:50 +0200

    add CLI parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,32 @@
 import Data.List
 import Data.Monoid
+import System.Console.CmdArgs
+import System.Environment
 
 import Evolutionary.Parser
 import Evolutionary.Data
 import Evolutionary.Algorithm
 import Evolutionary.Graphviz
 
+data Arguments = Arguments {
+	population :: Int,
+	steps :: Int
+} deriving (Show, Data, Typeable)
+
+defaults = Arguments {
+	population = 50,
+	steps = 500
+}
+
 main :: IO ()
 main = do
+	args <- cmdArgs defaults
 	nodes <- parseInput
 	let costMap = calculateAllEdges nodes
-	population <- createPopulation costMap nodes 50
-	let step = createNewPopulation costMap nodes 50
-	let repeated = iterateM step 500
-	p <- repeated population
+	let step = createNewPopulation costMap nodes (population args)
+	let repeated = iterateM step (steps args)
+	init <- createPopulation costMap nodes (population args)
+	p <- repeated init
 	let best = minimum p
 	putStrLn $ getGraph best 
 
